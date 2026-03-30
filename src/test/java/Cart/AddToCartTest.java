a6cc54fd3b2c07ec25637d25624b2ea6c716e2a9
madgiyas <muhgiyas@gmail.com>
2026-03-30 23:55:03 +0700

    assert

--- src/test/java/Cart/AddToCartTest.java
+++ src/test/java/Cart/AddToCartTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -82,5 +83,9 @@ public class AddToCartTest {
         ));
         // Open cart
         wait.until(ExpectedConditions.elementToBeClickable(By.id("show-your-cart"))).click();
+
+        WebElement cartItem = driver.findElement(By.cssSelector(".cart-item"));
+
+        Assert.assertTrue(cartItem.isDisplayed(), "Product was not added to cart");
     }
 }
